9e9bcabc7e4303ded2ab91cbb38400c217922714
minsun07 <142420604+minsun07@users.noreply.github.com>
2023-12-11 11:12:26 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main(){
+    int N;
+    scanf("%d", &N);
+    int arr[100];
+    int count = 0;
+    
+    for(int i=0; i<N; i++){
+        scanf("%d", &arr[i]);
+    }
+    int v;
+    scanf("%d", &v);
+    
+    for(int j=0; j<N; j++){
+        if(v == arr[j]){
+            count++;
+        }
+    }
+    printf("%d", count);
+}
