3032b66a4a7dc3952c467770c33d6f1f7662b4fc
minsun07 <142420604+minsun07@users.noreply.github.com>
2023-12-18 09:26:11 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 class Main{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-      
+        
         int arr[] = new int[9];
         int max = arr[0];
-        int k=0; //최댓값 위치를 담는 변수
-        
+        int indext=0;
         for(int i=0; i<arr.length; i++){
             arr[i] = sc.nextInt();
-            if(arr[i]>max){
+            if(max<arr[i]){
                 max = arr[i];
-                k = i+1;
+                indext = i + 1;
             }
-        }      
+        }
         System.out.println(max);
-        System.out.println(k);   
+        System.out.println(indext);
     }
 }
