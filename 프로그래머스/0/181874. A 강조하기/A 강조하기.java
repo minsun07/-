6f3744162906f5a9182d5a491d7a9e6c596f2866
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-05-10 22:14:51 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        String a = myString.toLowerCase();     
+        return answer = a.replace("a", "A");
+    }
+}
