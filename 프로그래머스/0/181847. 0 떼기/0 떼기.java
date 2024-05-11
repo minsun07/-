ab523d72b6a4bab5472a007a31c7976a01ee6dc6
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-05-11 18:10:44 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+       String answer = "";
+       int a = Integer.parseInt(n_str);
+        return answer = Integer.toString(a);
+    }
+}
