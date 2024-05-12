2ace18be0dc3d78c8b28d194e4160082235d06ce
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-05-12 23:52:59 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,6 +1,9 @@
 def solution(n):
     answer = []
+    # for문
+    # for i in range(1,n+1,2):
+    #     answer.append(i)
     
-    for i in range(1,n+1,2):
-        answer.append(i)
+    answer = [i for i in range(1, n+1, 2)]
+    
     return answer
