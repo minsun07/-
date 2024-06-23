2cec34c9b30569e7d1dbeb2c44a9d8bee5d21d72
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-06-23 22:13:13 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,7 +1,7 @@
 def solution(my_string):
     answer = ''
     for i in my_string:
-        if i.isupper():
+        if i.isupper(): #문자가 대문자인지 확인해주는 함수
             answer += i.lower()
         else:
             answer += i.upper()
