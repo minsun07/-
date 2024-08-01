37beaea4526e1d3607c08a4281322d08e777594a
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-08-01 20:53:36 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,5 @@
+def solution(n):
+    n = str(n)      # n을 문자열로 변환 : 그래야만 각 자리별로 나눌 수 있음
+    n_list = list(map(int,n)) # map함수로 리스트에 있는 각 원소들을 int형으로 바꾼 후 저장
+    n_list.reverse()   # 리스트를 역순으로 출력 
+    return n_list
