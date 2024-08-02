64f14d4d3e6040183568300da4cefcaca8efecbc
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-08-02 22:52:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i<absolutes.length; i++){
+            if(signs[i])  //true일 경우
+                answer+=absolutes[i];
+            else          //false일 경우(!signs[i])
+                answer-=absolutes[i];
+        }
+        return answer;
+    }
+}
