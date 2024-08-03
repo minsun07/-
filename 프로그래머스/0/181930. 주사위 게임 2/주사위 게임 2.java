7f8acd714ba05196dd16366f5f2b5206d89b0a8a
🤯Minsun <142420604+minsun07@users.noreply.github.com>
2024-08-03 19:31:05 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -3,25 +3,21 @@ class Solution {
         int not_same = a+b+c;
         int two_same = not_same*(a*a+b*b+c*c);
         int all_same = two_same*(a*a*a+b*b*b+c*c*c);
-        if(a==b){
-            if(a==c)
-                return all_same;
-            else
-                return two_same;
-        }
-        else if(a==c){
-            if(a==b)
-                return all_same;
-            else
-                return two_same;
-        }
-        else if(b==c){
-            if(b==a)
-                return all_same;
-            else
-                return two_same;
-        }
+        if(a==b&&a==c)
+            return all_same;
+        else if((a==b&&a!=c)||(a==c&&a!=b)||(b==c&&b!=a))
+            return two_same;
         else
             return not_same;
+    
+        
+        //다른 사람 풀이
+/*        int answer = a+b+c;
+        if(a==b&&b==c)             //모두 같을 경우
+            return answer*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        else if(a==b||b==c||c==a)  //두 개만 같을 경우
+            return answer*(a*a+b*b+c*c);
+        else                       //모두 다른 경우
+            return answer;*/
     }
 }
